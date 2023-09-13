9ace48c8f90a3b64240240955e89eb1e18dcbffa
zinaida.smirnova <zinaida.smirnova@jetbrains.com>
2023-09-13 16:18:42 +0200

    fix tests

--- src/test/java/zbsmirnova/restaurantvoting/service/AbstractServiceTest.java
+++ src/test/java/zbsmirnova/restaurantvoting/service/AbstractServiceTest.java
@@ -6,21 +6,8 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static zbsmirnova.restaurantvoting.util.ValidationUtil.getRootCause;
-
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
 @Sql(scripts = "classpath:data.sql", config = @SqlConfig(encoding = "UTF-8"))
-public abstract class AbstractServiceTest {
-    protected static final Logger log = getLogger("result");
-
-    public  static StringBuilder results = new StringBuilder();
-
-    @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Rule
-    public StopwatchImpl stopwatch = new StopwatchImpl();
-}
+public abstract class AbstractServiceTest { }
